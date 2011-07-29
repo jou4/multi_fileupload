474f1e070b9040b634319bb84cbc000c1b77389d
K. Kamitsukasa <jou4.kz@gmail.com>
2011-07-29 10:24:35 +0900

    update README

--- receive.php
+++ receive.php
@@ -8,7 +8,7 @@ if(empty($_FILES) || ! isset($_FILES[$file_key])){
 }
 
 // Save
-$id = md5(uniqid(mt_rand(), true));
+//$id = md5(uniqid(mt_rand(), true));
 /*
 $save_dir = 'files/';
 if( ! file_exists($save_dir)){
@@ -23,7 +23,7 @@ move_uploaded_file($_FILES[$file_key]['tmp_name'], $path);
 */
 
 // Log
-file_put_contents('debug.log', $id . "\n". print_r($_FILES, true) . "\n\n",  FILE_APPEND);
+//file_put_contents('debug.log', $id . "\n". print_r($_FILES, true) . "\n\n",  FILE_APPEND);
 
 echo('1');
 
